d345eb527a94bcb96b02b277e87ef0b3fc52b778
Willian da Silva <78602442+Williandasilvacode@users.noreply.github.com>
2021-05-30 21:23:51 -0300

    Adicionei os códigos

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Calculando o preço de um Carro alugado.
- Esse é um programa criado com a linguage de programação Python, que calcular o valor de um carro alugado, levando em consideração que, a diária custa R$60, e o Km rodado custa R$0,15.
+
+
+ Esse é um programa criado com a linguage de programação Python, escrito no editor de texto Visual Studio Code, que calcular o valor de um carro alugado, levando em consideração que, a diária custa R$60, e o Km rodado custa R$0,15.
---
+++ ex02.py
@@ -0,0 +1,11 @@
+﻿'''
+Escreva um programa que pergunte a quantidade de km percorridos por um carro alugado pelo usuário, assim como a quantidade de dias pelos quais o carro foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$ 60 por dia e R$ 0,15 por Km rodado.'''
+
+#Programa:
+
+km = int(input('Quantos km foram percorridos? '))
+dias = int(input('Por quantos dias ele foi alugado? '))
+
+preco = 60 * dias + 0.15 * km
+print('\n Km = {}. Dias:{}'.format(km, dias))#\n é apenas p/ cria um espeço entre as pergunta e a resposta.
+print('Valor a ser pago R$:{}'.format(preco))
